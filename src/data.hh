d41cb1bec69d53ba6a6c6a16b4ada391ff62e3f6
Guillaume Drapala-Bizouarn <guidrapbiz@hotmail.fr>
2018-07-04 12:18:08 +0200

    Ajout typedef pour les struct

--- src/data.hh
+++ src/data.hh
@@ -2,16 +2,16 @@
 #include <tuple>
 #include <vector>
 
-struct data {
+typedef struct data {
   float value;  // ex: 5538.2
   int date;     // ex: 32520 (-> 09h02)
-};
+} data;
 
-struct day {
+typedef struct day {
     std::vector<data> data_vect; // vector containing dots for 1 day
     std::string name;            // ex: CAC40
     std::string date;            // ex: 2018-05-14
-};
+} day;
 
 enum Type {
     DoubleTop,
