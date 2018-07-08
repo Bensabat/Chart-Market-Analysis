b21b80028150b5538106383ec351246ab874bef6
Guillaume Drapala-Bizouarn <guidrapbiz@hotmail.fr>
2018-07-09 00:11:51 +0200

    Add pattern to Makefile + complete main

--- src/main.cc
+++ src/main.cc
@@ -3,12 +3,22 @@
 #include <iostream>
 #include <vector>
 #include "parser.hh"
+#include "simple.hh"
+#include "pattern.hh"
 
 int main()
 {
 	// Vector containing all the days data
 	std::vector<day> days;
 	parse_days(days);	
-	
+  
+	for (auto day : days) 
+  {
+    auto simplified_day = day;
+    simplified_day.data_vect = simplification(day.data_vect);
+    auto patterns = getPatterns(simplified_day.data_vect);
+    printPattern(patterns);
+  }
+
 	return 0;
 }
